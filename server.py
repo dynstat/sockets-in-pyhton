11a8e3fee9d58941850e1b4b864f55db70d14886
dynstat <vs15vivek@gmail.com>
2022-10-16 12:23:08 +0530

    handling the exit!! command from client

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
 # import socket module
+from ast import arg
 import os
 import socket
 import sys
 import threading
+
+# # creating a object of socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_host = "127.0.0.1"
+server_port = 2005
+
+s.bind((server_host, server_port)) 
+
+s.listen()
+client_nickname = None
+
+print(f"SERVER is running at {server_host} on port {server_port}\n")
+
 
 SHUTDOWN = 0
 # function to handle the received data from the client continuously
@@ -27,39 +42,31 @@ def client_ne_kuch_bheja_kya(connected_client,nickname):
 def mssg_to_client(connected_client):
     global SHUTDOWN
     while True:
-        mssg_to_send = input("\rSERVER: ")
-        connected_client.send(mssg_to_send.encode())
+        
+        # mssg_to_send = input("\rSERVER: ")
         try:
-            if mssg_to_send != "exit!!":
-                connected_client.send(mssg_to_client.encode())
-            else:
-                print(f"SHUTTING THE SERVER DOWN")
-                connected_client.close()
-                s.close()
-                SHUTDOWN = 1
-                break
+            if connected_client:
+                mssg_to_send = input("\rSERVER: ")
+                if mssg_to_send != "exit!!" and connected_client:
+                    # print("in try")
+                    connected_client.send(mssg_to_send.encode())
+                else:
+                    print(f"SHUTTING THE SERVER DOWN (from mss_to_client func)")
+                    connected_client.close()
+                    # s.close()
+                    SHUTDOWN = 1
+                    break
         except Exception as e:
+            SHUTDOWN = 1
             print(f"Server closed because of client fault -> {e}")
+            connected_client.close()
             break
 
-
-# # creating a object of socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_host = "127.0.0.1"
-server_port = 2005
-
-s.bind((server_host, server_port)) 
-
-s.listen()
-client_nickname = None
-
-print(f"SERVER is running at {server_host} on port {server_port}\n")
-
 def main():
-    conn, addr = s.accept()
-    if conn:
-        handle_client(conn)
+    while True:
+        conn, addr = s.accept()
+        if conn:
+            handle_client(conn)
         
 def handle_client(conn):    
     client_nickname = conn.recv(1024).decode()
@@ -82,7 +89,15 @@ def handle_client(conn):
     client_thread_send.join()
 
 if __name__ == "__main__":
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    
+    count = 1
     while True:
-        main()
+        print(f"main number {count}, shutdown value = {SHUTDOWN}")
+        count +=1
+        # main()
         if SHUTDOWN:
-            os._exit()
+            print("Shutting down (from main)")
+            sys.exit(1)
+            
